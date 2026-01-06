25dc83493a08a320e3ba67a27573c29b7c83d94e
Kreyno <michael.wagener@neuefische.de>
2026-01-06 11:21:54 +0100

    reformatting the code to restore previous state

--- main.py
+++ main.py
@@ -5,19 +5,19 @@
 
 
 def askTheUser():
-    # try:
-    userInput = float(input("Please enter a number to divide 100 by: "))
-    # except ValueError:
-    #     print("That was not a valid number. Please try again.")
-    #     return askTheUser()
+    try:
+        userInput = float(input("Please enter a number to divide 100 by: "))
+    except ValueError:
+        print("That was not a valid number. Please try again.")
+        return askTheUser()
     return userInput
 
 
 def divide100ByUserInput():  # 100 / by the user Input || Example : 100 / 25 = 4
     userInput = askTheUser()
-    # result = 100 / userInput  # User Input = 25
-    return 100 / userInput
-    # return result
+    result = 100 / userInput  # User Input = 25
+    return result
+
 
 result = divide100ByUserInput()
 
