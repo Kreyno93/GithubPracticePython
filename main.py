d20d169e30d98935fa5f593e3e5d38c85d4b50a3
Kreyno <michael.wagener@neuefische.de>
2026-01-06 11:17:28 +0100

    add comments to better understand the code

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 # What can go wrong?
+# I need to divide a number by the user input
+
+# Where is my entry point ?
 
 
 def askTheUser():
-    try:
-        userInput = float(input("Please enter a number to divide 100 by: "))
-    except ValueError:
-        print("That was not a valid number. Please try again.")
-        return askTheUser()
+    # try:
+    userInput = float(input("Please enter a number to divide 100 by: "))
+    # except ValueError:
+    #     print("That was not a valid number. Please try again.")
+    #     return askTheUser()
     return userInput
 
 
-def divide100ByUserInput():
+def divide100ByUserInput():  # 100 / by the user Input || Example : 100 / 25 = 4
     userInput = askTheUser()
-    result = 100 / userInput
-    return result
-
-
-result100byNumber = divide100ByUserInput()
-
-print(result100byNumber)
+    # result = 100 / userInput  # User Input = 25
+    return 100 / userInput
+    # return result
